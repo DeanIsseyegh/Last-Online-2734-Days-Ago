3d52547007747e590f6252161b7519acb46e1ed4
IbrahimHamwi <ibrahimhamwi_619@hotmail.com>
2022-03-20 22:28:58 +0300

    IntroScene done

--- Assets/Scripts/IntroScreen/SwitchFromTitleToDevScreen.cs
+++ Assets/Scripts/IntroScreen/SwitchFromTitleToDevScreen.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class SwitchFromTitleToDevScreen : MonoBehaviour
@@ -13,9 +14,8 @@ public class SwitchFromTitleToDevScreen : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GoToMainHubScene()
     {
-        
+        SceneManager.LoadScene("Main Hub");
     }
 }
