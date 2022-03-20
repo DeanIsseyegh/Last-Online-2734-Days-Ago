9fa3e873db0b842bac6940fd44133f46ca794c3d
Dean Isseyegh <issdea@gmail.com>
2022-03-20 19:30:43 +0000

    Get subscription popup cutscene working

--- Assets/Scripts/Cutscenes/InitialCutSceneManager.cs
+++ Assets/Scripts/Cutscenes/InitialCutSceneManager.cs
@@ -1,16 +1,21 @@
 using Player;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class InitialCutSceneManager : MonoBehaviour
+public class InitialCutSceneManager : MonoBehaviour, IPointerClickHandler
 {
+    [SerializeField] private GameObject subscriptionPopup;
     [SerializeField] private GameObject player;
     [SerializeField] private GameObject firstWalkToPoint;
     private PlayerController _playerController;
     private CutsceneState _cutsceneState = CutsceneState.INITIAL_WALK;
 
+    private bool _hasClickedPlayTrial;
+
     enum CutsceneState
     {
-        INITIAL_WALK
+        INITIAL_WALK,
+        SUBSCRIPTION_POPUP
     }
 
     private void Awake()
@@ -29,14 +34,33 @@ public class InitialCutSceneManager : MonoBehaviour
     {
         if (_cutsceneState == CutsceneState.INITIAL_WALK)
         {
-            if (Vector2.Distance(player.transform.position, firstWalkToPoint.transform.position) <= 0.1f)
+            if (Vector2.Distance(player.transform.position, firstWalkToPoint.transform.position) > 0.1f)
             {
                 _playerController.dir = Vector2.down * Time.deltaTime;
             }
             else
             {
-                
+                _playerController.dir = Vector2.zero;
+                _cutsceneState = CutsceneState.SUBSCRIPTION_POPUP;
+                subscriptionPopup.SetActive(true);
             }
+        }
+
+        if (_cutsceneState == CutsceneState.SUBSCRIPTION_POPUP)
+        {
+            if (_hasClickedPlayTrial)
+            {
+                subscriptionPopup.SetActive(false);
+            }
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            Debug.Log("Clicked " + name);
+            _hasClickedPlayTrial = true;
         }
     }
 }
